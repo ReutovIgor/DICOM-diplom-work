6abcae2d3c9087ce799f9860e0b243482922b945
ReutovIgor <ireutov_777@rambler.ru>
2015-05-28 10:59:49 +0300

    Huge change

--- DICOM/DICOM/Form1.cs
+++ DICOM/DICOM/Form1.cs
@@ -8,11 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using openDicom.Registry;
-using openDicom.File;
-using openDicom.DataStructure;
-using openDicom.DataStructure.DataSet;
-using openDicom.Image;
+using EvilDICOM;
+using EvilDICOM.Core;
+using EvilDICOM.Core.Dictionaries;
+using EvilDICOM.Core.Element;
+using EvilDICOM.Core.Enums;
+using EvilDICOM.Core.Extensions;
+using EvilDICOM.Core.Helpers;
+using EvilDICOM.Core.Interfaces;
+using EvilDICOM.Core.IO;
+using EvilDICOM.Core.Logging;
+using EvilDICOM.Core.Modules;
+using EvilDICOM.Core.Selection;
+
 
 
 namespace DICOM
@@ -33,75 +41,17 @@ namespace DICOM
             {
                 fileName = diag.FileName;
             }
-            
-            DataElementDictionary dataElementDictionary = new DataElementDictionary ();
-            UidDictionary uidDictionary = new UidDictionary();
-            string elementsLOcation = @"C:\Users\Vextrell\Desktop\DICOM\DICOM\DICOM-LIbrary\opendicom-sharp\dd\dicom-elements-2007.dic";
-            string uidsLocation = @"C:\Users\Vextrell\Desktop\DICOM\DICOM\DICOM-LIbrary\opendicom-sharp\dd\dicom-uids-2007.dic";
-            try
-            {
-                dataElementDictionary.LoadFrom(elementsLOcation,
-                    DictionaryFileFormat.BinaryFile);
-                uidDictionary.LoadFrom(uidsLocation,
-                    DictionaryFileFormat.BinaryFile);
-            }
-            catch (Exception dictionaryException)
-            {
-                Console.Error.WriteLine("Problems processing dictionaries:\n" +
-                    dictionaryException);
-                return;
-            }
-            
-            AcrNemaFile file = null;
-            try
-            {
-                if (DicomFile.IsDicomFile(fileName))
-                    file = new DicomFile(fileName, false);
-                else if (AcrNemaFile.IsAcrNemaFile(fileName))
-                    file = new AcrNemaFile(fileName, false);
-                else
-                    Console.Error.WriteLine("Selected file is wether a " +
-                        "DICOM nor an ACR-NEMA file.");
-            }
-            catch (Exception dicomFileException)
-            {
-                Console.Error.WriteLine("Problems processing DICOM file:\n" +
-                    dicomFileException);
-                return;
-            }
-            bool flag = file.HasPixelData;
-            openDicom.Image.PixelData obraz = new openDicom.Image.PixelData(file.DataSet);
 
-            byte[][] pixelimage = obraz.ToBytesArray();
-            int dicomHeight = obraz.Rows;
-            int dicomWidth = obraz.Columns;
-            //obraz
-
-            List<byte> bytes = new List<byte>(); // this list should be filled with values
-            int width = 100;
-            int height = 100;
-            int bpp = 24;
-
-            Bitmap bmp = new Bitmap(width, height);
-            //bmp = new Bitmap()
-            for (int y = 0; y < dicomHeight; y++)
-            {
-                for (int x = 0; x < dicomWidth; x++)
-                {
-                    int i = ((y * dicomWidth) + x) * (bpp / 8);
-                    if (bpp == 24) // in this case you have 3 color values (red, green, blue)
-                    {
-                        // first byte will be red, because you are writing it as first value
-                        byte r = pixelimage[i][0];
-                        byte g = pixelimage[i + 1][0];
-                        byte b = pixelimage[i + 2][0];
-                        Color color = Color.FromArgb(r, g, b);
-                        bmp.SetPixel(x, y, color);
-                    }
-
-                }
-            }
+            var file = DICOMObject.Read(fileName);
+            var stream = file.PixelStream;
+            Image img = Image.FromStream(stream);
+            Bitmap bt = new Bitmap(stream);
+            this.pictureBox1.Image = bt;
+            //var imgFile = new ImageMatrix(fileName);
+            //pictureBox1.Image = imgFile.GetImage(0);
             
         }
+
+
     }
 }
